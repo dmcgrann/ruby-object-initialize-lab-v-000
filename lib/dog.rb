ac5172eb06be31a9f6db20ac75c80a1ed1008f4c
David McGrann <dpmcgrann@gmail.com>
2018-02-01 23:50:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 class Dog
-  def initialize(name, breed)
-    @breed = breed
+
+  def initialize(name, breed= "Mutt")
     @name = name
+    @breed = breed
   end
-  def breed=(breed)
-    @breed
+
+  def name
+    @name
   end
+
   def breed
     @breed
   end
